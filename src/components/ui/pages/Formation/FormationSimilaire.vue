6f20b6bea3def0de6f5e7719efdea9a8f3a53335
anas <anasmpasti@hotmail.com>
2021-04-05 14:40:07 +0100

    apply clean code

--- src/components/ui/pages/Formation/FormationSimilaire.vue
+++ src/components/ui/pages/Formation/FormationSimilaire.vue
@@ -1,50 +1,46 @@
 <template>
   <div class="formation-similaire bg_light_2" id="formationSimilaire">
-
     <div class="container-fluid pt-5" v-if="isLoaded">
-      <h1 class="text-center text-dark font-lg-s8 font-md-s6 font-s6 text_bold py-3">
+      <h1
+        class="text-center text-dark font-lg-s8 font-md-s6 font-s6 text_bold py-3"
+      >
         Formations similaire sur ❝{{ category }}❞
       </h1>
       <!-- card-formation -->
-      <formation-card 
-        :formations="formations.slice(0, 4)">
-      </formation-card>
+      <formation-card :formations="formations.slice(0, 4)"> </formation-card>
       <!-- card-formation -->
     </div>
     <!-- ELSE LOADING .. -->
     <div v-else class="loading loading_md">
-      <img src="../../../../assets/img/loading2.gif" class="loading_img">
+      <img src="../../../../assets/img/loading2.gif" class="loading_img" />
     </div>
     <!-- END-LOADING .. -->
 
     <div class="line">
       <hr />
     </div>
-
   </div>
-
 </template>
 
 <script>
-import FormationCard from '../../../ui/shared/FormationCard.vue'
+import FormationCard from "../../../ui/shared/FormationCard.vue";
 
 export default {
-  name: 'FormationSimilaire',
+  name: "FormationSimilaire",
   components: {
-    FormationCard
+    FormationCard,
   },
   data() {
     return {
-      form_param: undefined
-    }
+      form_param: undefined,
+    };
   },
   props: {
     formations: { type: Array, required: true },
     category: String,
-    isLoaded: Boolean
+    isLoaded: Boolean,
   },
-  methods: {
-  }
-}
+  methods: {},
+};
 </script>
 
